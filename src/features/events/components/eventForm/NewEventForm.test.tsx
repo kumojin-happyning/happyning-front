fbc22cdc11dc1c6db4908ed71aa3d8e83ef78ef8
0h My Goat <a.devos@octocorn.fr>
2024-01-18 15:01:07 +0100

    chore: exclusion fichiers coverage :white_check_mark:

--- src/features/events/components/eventForm/NewEventForm.test.tsx
+++ src/features/events/components/eventForm/NewEventForm.test.tsx
@@ -1,15 +1,32 @@
-import {render} from "@testing-library/react";
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import NewEventDialog from './NewEventForm.component';
+import EventService from '../../services/Event.service';
 import EventModel from "../../models/Event.model";
-import NewEventFormComponent from "./NewEventForm.component";
+
+jest.mock('../../services/Event.service');
 
 describe('NewEventForm', () => {
+    let mockSetDialogVisible: jest.Mock
 
-    it('should render successfully', () => {
-        const {baseElement} = render(<NewEventFormComponent
-            events={[] as EventModel[]}
-            isDialogVisible={false}
-            setDialogVisible={() => {}}
-        />);
-        expect(baseElement).toBeTruthy();
+    jest.mock('../../services/Event.service');
+
+    beforeEach(() => {
+        mockSetDialogVisible = jest.fn();
     });
+
+    it('renders without crashing', () => {
+        render(<NewEventDialog events={[]} isDialogVisible={true} setDialogVisible={mockSetDialogVisible} />);
+    });
+
+    it('submits the form', async () => {
+
+        const { getByText } = render(<NewEventDialog events={[]} isDialogVisible={true} setDialogVisible={mockSetDialogVisible} />);
+
+        fireEvent.click(getByText('Créer'));
+
+        await waitFor(() => expect(EventService.createEvent).toHaveBeenCalled());
+    });
+
 });
